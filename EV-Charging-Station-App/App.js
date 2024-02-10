8d737d6a6e7e15b265eeb2b5c1c794f7b79fb95d
nikhilchalis <nikhilchalis@gmail.com>
2024-02-10 22:26:24 +1100

    some more changes

--- EV-Charging-Station-App/App.js
+++ EV-Charging-Station-App/App.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:50
   },
 });
+ 
